4bfb3c47528c8e27640c5766991b8979f26b4b41
DESKTOP-L8AHJ5I\Ashiq <ashiqul.eee@gmail.com>
2022-04-04 14:59:09 +0600

    Chart UI done

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -10,60 +10,23 @@ import {
   BarChart,
   Legend,
   Bar,
+  Line,
 } from "recharts";
 
-const data = [
-  {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-];
-
 const Dashboard = () => {
-  const [datas, setDatas] = useState([]);
+  const [data, setData] = useState([]);
   useEffect(() => {
     fetch("data.json")
       .then((response) => response.json())
-      .then((data) => setDatas(data));
+      .then((data) => setData(data));
   }, []);
+  console.log(data);
 
   return (
     <div className="chart-container">
       <div className="mx-auto">
         <AreaChart
-          width={550}
+          width={500}
           height={400}
           data={data}
           margin={{
@@ -74,32 +37,32 @@ const Dashboard = () => {
           }}
         >
           <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
+          <XAxis dataKey="month" />
           <YAxis />
           <Tooltip />
+          <Legend />
           <Area
             type="monotone"
-            dataKey="uv"
-            stackId="1"
-            stroke="#8884d8"
-            fill="#8884d8"
+            dataKey="investment"
+            stroke="#9900FF"
+            fill="#9850FF"
           />
           <Area
             type="monotone"
-            dataKey="pv"
-            stackId="1"
-            stroke="#82ca9d"
-            fill="#82ca9d"
+            dataKey="revenue"
+            stroke="#009999"
+            fill="#009999"
           />
+
           <Area
             type="monotone"
-            dataKey="amt"
-            stackId="1"
-            stroke="#ffc658"
-            fill="#ffc658"
+            dataKey="sell"
+            stroke="#FFD54F"
+            fill="#FFD54F"
           />
         </AreaChart>
       </div>
+
       <div className="mx-auto">
         <BarChart
           width={600}
@@ -113,12 +76,13 @@ const Dashboard = () => {
           }}
         >
           <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
+          <XAxis dataKey="month" />
+          <YAxis dataKey="investment" />
           <Tooltip />
           <Legend />
-          <Bar dataKey="pv" fill="#8884d8" />
-          <Bar dataKey="uv" fill="#82ca9d" />
+          <Bar dataKey="revenue" fill="#8884d8" />
+          <Bar dataKey="sell" fill="#82ca9d" />
+          <Bar dataKey="investment" fill="#3399FF" />
         </BarChart>
       </div>
     </div>
